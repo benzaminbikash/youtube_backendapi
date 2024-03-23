451b32fa038305f5058844e9d4ead13201945c04
Bikash Nepali <benzaminbikash@gmail.com>
2024-03-23 17:23:56 +0545

    cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDING_CLOUD_NAME,
+  api_key: process.env.CLOUDING_API_KEY,
+  api_secret: process.env.CLOUDING_API_SECRET,
+});
+
+const uploadOnCloudinary = async (uploadFile) => {
+  try {
+    if (!uploadFile) return null;
+    const response = await cloudinary.uploader.upload(uploadFile, {
+      resource_type: "auto",
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+    fs.unlinkSync(uploadFile);
+    return null;
+  }
+};
+
+module.exports = uploadOnCloudinary;
